55a1288fd50ec537c0333f45eb0a003fecf36097
pathawks <pathawks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-26 16:53:55 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/dirtysuds-category-thumbnail/trunk@351917 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== DirtySuds - Category Thumbnail ===
+Contributors: Pat Hawks
+Tags: plugins, wordpress, shortcode, category, thumbnail, image
+Requires at least: 3.0
+Tested up to: 3.1
+Stable tag: trunk
+
+Adds shortcode [catthumb] to embed a thumbnail image for a category.
+
+== Description ==
+
+[catthumb id="Category ID" link="true"] will find the most recent post in the specified Category with a featured image and return the thumbnail for that image. If **link="true"** is specified, the image will link to the category specified.
+
+== Installation ==
+
+1. Upload 'dirtysuds-category-thumbnail' to the '/wp-content/plugins/' directory
+2. Activate **DirtySuds - Category Thumbnail** through the 'Plugins' menu in WordPress
+
+
+== Changelog ==
+
+= 1.00.20110226 =
+* First version
+* Works
---
+++ thumb.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: DirtySuds - Category Thumbnail
+Plugin URI: http://dirtysuds.com
+Description: Adds shortcode to embed a category thumbnail image link
+Author: Pat Hawks
+Version: 1.00.20110226
+Author URI: http://www.pathawks.com
+
+Updates:
+1.00.20110226 - First Version
+
+  Copyright 2011 Pat Hawks  (email : pat@pathawks.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// [catthumb id="Category ID" link="ture"]
+
+function dirtysuds_category_thumb( $atts ) {
+	extract( shortcode_atts( array(
+		'id' => 1,
+		'link' => 'true',
+	), $atts ) );
+	
+	$embed = '';
+	$posts = get_posts('cat='.$id.'&showposts=1&meta_key=_thumbnail_id');
+	foreach( $posts as $post ) : setup_postdata($post);
+		if ($link == 'true') {
+			$embed = '<a href="'.get_category_link($id).'" title="'.get_cat_name($id).'">';
+		}
+		$imageid = get_post_thumbnail_id($post->ID);
+		$embed .= '<img src="'.wp_get_attachment_thumb_url($imageid).'" alt="'.get_the_title($imageid).'" />';
+		if ($link == 'true') {$embed .= '</a>';}
+	endforeach;
+	
+	return $embed;
+}
+
+add_shortcode( 'catthumb', 'dirtysuds_category_thumb' );
