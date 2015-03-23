7c2696f883d811d9a77c75b776f20cd6760c8c4a
pathawks <pathawks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-23 16:01:24 +0000

    Added options for custom post types and featured image size
    git-svn-id: https://plugins.svn.wordpress.org/dirtysuds-category-thumbnail/trunk@1118930 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 
 `[catthumb id="CategoryID" link="true"]` will find the most recent post in the specified Category with a featured image and return the thumbnail for that image. If *link="true"* is specified, the image will link to the category specified.
 
+You can optionally search custom post types with the `post_type` attribute, and specify the thumbnail size with the `size` attribute. IE
+`[catthumb id="CategoryID" link="true" post_type="restaurants" size="full"]`
+
 The plugin will only search *featured images*, not *attached images*. It will not look for images embedded in the post itself, or other attached images. If a category does not have any posts with featured images, this plugin will not return a thumbnail image, even if the category has posts with attached images.
--- thumb.php
+++ thumb.php
@@ -26,24 +26,27 @@ Version: 1.03
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// [catthumb id="Category ID" link="ture"]
+// [catthumb id="Category ID" link="true"]
 
 function dirtysuds_category_thumb( $atts ) {
 	extract( shortcode_atts( array(
 		'id' => 1,
 		'link' => 'true',
+		'post_type' => 'post',
+		'size' => 'thumbnail'
 	), $atts ) );
-
 	$embed = get_transient( 'dirtysuds_category_thumb' . $id . $link );
 	if( $embed ) return $embed;
 
 	$embed = '';
-	$posts = get_posts('cat='.$id.'&showposts=1&meta_key=_thumbnail_id');
-	if( $posts[0] ) {
-		$embed = get_the_post_thumbnail($posts[0]->ID,'thumbnail');
+	$posts = get_posts('cat='.$id.'&post_type='.$post_type.'&showposts=1&meta_key=_thumbnail_id');
+	if( isset( $posts[0] ) ) {
+		$embed = get_the_post_thumbnail($posts[0]->ID, $size);
 		if ($link == 'true') {
 			$embed = '<a href="'.get_category_link($id).'" title="'.get_cat_name($id).'">' . $embed . '</a>';
 		}
+	} else {
+		return '';
 	}
 	set_transient( 'dirtysuds_category_thumb' . $id . $link, $embed, 5 * MINUTE_IN_SECONDS );
 
